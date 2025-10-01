1370317818867fb9503184c496478f1064155617
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-01 16:58:17 +0530

    modified homepage to display notes card

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
   <div class="container mx-auto p-4">
     <div class="heading text-xl font-medium">Your Notes</div>
-    <div class="sub-heading text-gray-500">6 notes</div>
+    <div class="sub-heading text-gray-500 mb-4">6 notes</div>
+    @for ($i = 0; $i < 6; $i++)
+      <x-note-card />
+    @endfor
   </div>
 </x-layout>
