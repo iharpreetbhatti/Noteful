412fc9a60747bef3fda4b85a93881132d2f297b9
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-01 16:57:35 +0530

    created notes card component

---
+++ resources/views/components/note-card.blade.php
@@ -0,0 +1,10 @@
+<div class="note-card bg-amber-100 rounded-2xl border border-gray-300 p-4 mb-4 hover:shadow-md">
+  <h3 class="note-title text-lg font-medium mb-2">Meeting Notes</h3>
+  <p class="note-description text-gray-800 text-sm mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+    exercitationem
+    sequi
+    deleniti
+    beatae suscipit officiis, perspiciatis eaque aut illum est impedit nobis minima! Ex ratione quo libero, deserunt
+    mollitia nisi?</p>
+  <span class="note-date text-sm"><i class="fa-regular fa-calendar"></i> Oct 1, 2025</span>
+</div>
