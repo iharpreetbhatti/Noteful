8164b0a170e714034b1c9e67afb42d02608eeb45
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-01 16:20:06 +0530

    created site homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <div class="container mx-auto p-4">
+    <div class="heading text-xl font-medium">Your Notes</div>
+    <div class="sub-heading text-gray-500">6 notes</div>
+  </div>
+</x-layout>
