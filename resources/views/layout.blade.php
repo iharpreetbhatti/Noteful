8eb9e80edc56241ed54f129aef149260f853a6e7
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-01 16:18:05 +0530

    created basic layout structure

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+    integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>{{ $title ?? 'Noteful - Your Personal Notetaking App' }}</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 min-h-screen">
+  <x-header />
+  {{ $slot }}
+</body>
+
+</html>
