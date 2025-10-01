aa8b8e90dd60179fbe7e716bed9ac5c21c95da1a
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-01 16:18:52 +0530

    created site header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header class="bg-white border-b border-gray-300 mb-4">
+  <div class="container mx-auto p-4 flex justify-between items-center">
+    <a href="/" class="font-bold text-2xl"><i class="fa-regular fa-note-sticky"></i> Noteful</a>
+    <a href="#" class="p-2 rounded-md bg-black text-white"><i class="fa-solid fa-plus"></i> <span
+        class="hidden lg:inline">New Note</span></a>
+  </div>
+</header>
