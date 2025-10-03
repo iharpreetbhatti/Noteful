0d93c702e06f53603f45b705bdc292209b8d0c9d
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-03 13:09:27 +0530

    added route to handle dashboard get requests

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
