2da814736b044f2aef04a24245c217ee18623d83
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-04 12:16:32 +0530

    recreated Note controller with '--resource' command

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('notes', NoteController::class);
