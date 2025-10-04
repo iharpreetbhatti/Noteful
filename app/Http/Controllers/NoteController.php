8473abe3662d11e742210d279acbbdfb5fe551fe
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-04 13:11:05 +0530

    added logic to handle get requests for single note view

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -37,9 +37,9 @@ class NoteController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Note $note): View
     {
-        //
+        return view('notes.show', compact('note'));
     }
 
     /**
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -2,7 +2,9 @@
   <div class="container mx-auto p-4">
 
     @forelse ($notes as $note)
-      <x-note-card :note="$note" />
+      <a href="{{ route('notes.show', $note->id) }}">
+        <x-note-card :note="$note" />
+      </a>
     @empty
       <p class="text-gray-600">No notes available.</p>
     @endforelse
