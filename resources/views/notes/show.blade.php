35d57862b95ac75201040686738847beee571254
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-04 13:07:09 +0530

    added intial view for single note page

---
+++ resources/views/notes/show.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <div class="container mx-auto p-4">
+    <h2 class="text-2xl font-bold my-4">{{ $note->title }}</h2>
+    <p class="text-gray-800 mb-4">{{ $note->description }}</p>
+  </div>
+</x-layout>
