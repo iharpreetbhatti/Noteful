f8f3fa44b881d79434faa1cb604e9548dbb24d05
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-09 14:41:44 +0530

    replaced form inputs with components

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,18 +1,13 @@
 <x-layout>
-  <div class="container mx-auto mb-4 p-2 bg-white">
+  <div class="mx-auto mb-4 p-8 rounded-lg md:w-3xl bg-white">
     <form action="/notes" method="POST">
       @csrf
-      <input type="text" name="title" placeholder="Note Title" class="border p-2 w-full mb-2"
+      <x-inputs.text name="title" placeholder="Note Title" class="p-2 w-full mb-2 focus:outline-none"
         value="{{ old('title') }}" />
-      <!-- Error Message for Title -->
-      @error('title')
-        <div class="text-red-500 mb-2 text-sm">{{ $message }}</div>
-      @enderror
-      <textarea name="description" placeholder="Start writing your note" class="border p-2 w-full mb-2">{{ old('description') }}</textarea>
-      @error('description')
-        <div class="text-red-500 mb-2 text-sm">{{ $message }}</div>
-      @enderror
-      <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded"><i class="fa-regular fa-floppy-disk"></i>
+      <x-inputs.textarea name="description" placeholder="Start writing your note"
+        class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description') }}" />
+
+      <button type="submit" class="bg-black text-white px-4 py-2 rounded"><i class="fa-regular fa-floppy-disk"></i>
         Save</button>
     </form>
   </div>
