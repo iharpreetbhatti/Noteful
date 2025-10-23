4019a7a70bd80e0e854c09a242fbc9d14b53567a
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-23 11:37:05 +0530

    fixed width from container to 4xl for large screens and above

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,6 +1,6 @@
 @props(['hook', 'lead'])
 
-<section class="hero container mx-auto py-16 px-4 text-center">
+<section class="hero lg:w-4xl mx-auto py-16 px-4 text-center">
   <h1 class="hook text-6xl font-bold bg-gradient-to-r from-gray-900 to-blue-600 bg-clip-text text-transparent mb-6">
     {{ $hook }}</h1>
   <p class="lead text-2xl text-gray-500 mb-6">{{ $lead }}</p>
