f2661474521223e13a34e9521774a1729954f54f
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-23 10:32:53 +0530

    altered features section to use section layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,11 @@
 <x-home-layout>
   <x-hero hook="Your thoughts, beautifully organized"
     lead="Capture ideas, organize thoughts, and never lose track of what matters. Noteful makes note-taking effortless and enjoyable." />
-  <x-features />
+  {{-- <x-features /> --}}
+  <x-section-layout id="features" heading="Everything you need to stay organized"
+    subHeading="Powerful features designed to make note-taking simple, beautiful, and efficient">
+    <x-features />
+  </x-section-layout>
   <x-section-layout id="testimonials" heading="Loved by thousands of users"
     subHeading="See what people are saying about Noteful">
     <x-testimonials />
