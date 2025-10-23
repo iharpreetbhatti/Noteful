5f5ced6a0748eeab43dcd702b601f2d9d4a77e30
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-23 11:43:16 +0530

    fixed margins and font size

--- resources/views/components/pageheading.blade.php
+++ resources/views/components/pageheading.blade.php
@@ -1,6 +1,6 @@
 @props(['heading', 'subheading'])
-<div class="page-heading mb-5">
-  <h1 class="text-2xl font-bold mb-2">{{ $heading }}</h1>
+<div class="page-heading mt-6 mb-8">
+  <h1 class="text-3xl font-bold mb-2">{{ $heading }}</h1>
   @if ($subheading)
     <p class="text-gray-600">{{ $subheading }}</p>
   @endif
