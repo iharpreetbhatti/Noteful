cf30ecfc07e935c2f70c7e9818d20462d021ed4a
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-24 12:47:55 +0530

    divided routes into middleware groups for auth and guest middleware

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,26 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\NoteController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware('guest')->group(function () {
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+  Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('notes', NoteController::class);
+  Route::get('/register', [RegisterController::class, 'register'])->name('register');
 
-Route::get('/register', [RegisterController::class, 'register'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+  Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+  Route::get('/login', [LoginController::class, 'login'])->name('login');
+
+  Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+
+});
+
+Route::middleware('auth')->group(function () {
+
+  Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+  Route::resource('notes', NoteController::class);
+
+  Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+});
