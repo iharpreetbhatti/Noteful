ca7565db845293a89ce0ad139316da267390d34f
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-24 12:50:14 +0530

    fixed recent notes presentation

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,14 +1,19 @@
 <x-layout>
   <div class="container mx-auto p-4">
     <div class="flex justify-between items-center mb-4">
-      <h2 class="heading text-xl font-medium"><i class="fa-regular fa-clock"></i> Recent Notes</h2>
+      <h2 class="heading text-xl font-medium"><i class="fa-regular fa-clock"></i> Your Recent Notes</h2>
       <a href="/notes" class="p-2 rounded-md inline-block border border-gray-300 bg-white hover:bg-gray-100">
         View All Notes</a>
     </div>
-    @forelse ($notes as $note)
-      <x-note-card :note="$note" />
-    @empty
-      <p class="text-gray-500">No recent notes available.</p>
-    @endforelse
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      @forelse ($notes as $note)
+        <a href="{{ route('notes.show', $note->id) }}" class="">
+          <x-note-card :note="$note" />
+        </a>
+      @empty
+        <p class="text-gray-500">No recent notes available.</p>
+      @endforelse
+    </div>
+
   </div>
 </x-layout>
