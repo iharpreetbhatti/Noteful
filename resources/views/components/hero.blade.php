7aa2aab0099fd1f1371cff7ccae8589f9ebd24c3
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-25 11:45:53 +0530

    updated to use use button link component and made minor adjustments to spacing and text

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,18 +1,14 @@
 @props(['hook', 'lead'])
 
-<section class="hero lg:w-4xl mx-auto py-16 px-4 text-center">
-  <h1 class="hook text-6xl font-bold bg-gradient-to-r from-gray-900 to-blue-600 bg-clip-text text-transparent mb-6">
+<section class="hero md:w-3xl mx-auto py-16 px-4 text-center">
+  <h1 class="hook text-5xl font-bold bg-gradient-to-r from-gray-900 to-blue-600 bg-clip-text text-transparent mb-6">
     {{ $hook }}</h1>
-  <p class="lead text-2xl text-gray-500 mb-6">{{ $lead }}</p>
+  <p class="lead text-xl text-gray-500 mb-8">{{ $lead }}</p>
 
   <div class="flex flex-col sm:flex-row items-center justify-center gap-4 w-full">
-    <a href="/register"
-      class="py-2 px-4 rounded-md bg-black hover:bg-gray-900 text-white font-medium text-md text-center w-full sm:w-auto">
-      Get Started Free
-    </a>
-    <a href="/login"
-      class="py-2 px-4 rounded-md bg-white text-black hover:bg-gray-100 border border-gray-200 font-medium text-md text-center w-full sm:w-auto">
-      Sign In
-    </a>
+    <x-button-link text="Get Started Free" url="{{ route('register') }}"
+      classNames="py-2 px-8 bg-black hover:bg-gray-900 text-white font-medium text-lg w-full sm:w-auto" />
+    <x-button-link text="Sign In" url="{{ route('login') }}"
+      classNames="py-2 px-8 bg-white text-black hover:bg-gray-100 border border-gray-200 font-medium text-lg w-full sm:w-auto" />
   </div>
 </section>
