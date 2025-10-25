a451487494d2275b264eedde1f48e59b83158bcf
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-25 12:22:51 +0530

    replaced 'all notes' link with button link component

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,8 +2,8 @@
   <div class="container mx-auto p-4">
     <div class="flex justify-between items-center mb-4">
       <h2 class="heading text-xl font-medium"><i class="fa-regular fa-clock"></i> Your Recent Notes</h2>
-      <a href="/notes" class="p-2 rounded-md inline-block border border-gray-300 bg-white hover:bg-gray-100">
-        View All Notes</a>
+      <x-button-link url="{{ route('notes.index') }}" text="View All Notes"
+        classNames="px-4 py-2 text-sm font-medium inline-block border border-gray-300 bg-white hover:bg-gray-100" />
     </div>
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       @forelse ($notes as $note)
