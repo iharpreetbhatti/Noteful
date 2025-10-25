f769109c5395a3c65f5c6f40bd9b7ef27ea5f80b
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-25 13:18:04 +0530

    added page heading component to dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,7 +1,11 @@
+@php
+  $firstName = explode(' ', auth()->user()->name)[0];
+@endphp
 <x-layout>
   <div class="container mx-auto p-4">
+    <x-pageheading heading="Good morning, {{ $firstName }}!" subheading="Ready to capture your thoughts today?" />
     <div class="flex justify-between items-center mb-4">
-      <h2 class="heading text-xl font-medium"><i class="fa-regular fa-clock"></i> Your Recent Notes</h2>
+      <h2 class="heading text-xl font-medium"><i class="fa-regular fa-clock"></i> My Recent Notes</h2>
       <x-button-link url="{{ route('notes.index') }}" text="View All Notes"
         classNames="px-4 py-2 text-sm font-medium inline-block border border-gray-300 bg-white hover:bg-gray-100" />
     </div>
