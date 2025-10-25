cbe6657b60528b6128bb76d92418ed8bd9af9b11
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-25 10:20:48 +0530

    fixed date formatting

--- resources/views/components/note-card.blade.php
+++ resources/views/components/note-card.blade.php
@@ -2,5 +2,5 @@
 <div class="note-card bg-white rounded-2xl border border-gray-300 p-4 hover:shadow-md">
   <h3 class="note-title text-lg font-medium mb-2">{{ $note->title }}</h3>
   <p class="note-description text-gray-800 text-sm mb-2">{{ $note->description }}</p>
-  <span class="note-date text-sm"><i class="fa-regular fa-calendar"></i> {{ $note->updated_at->format('F d, Y') }}</span>
+  <span class="note-date text-sm"><i class="fa-regular fa-calendar"></i> {{ $note->updated_at->format('M d, Y') }}</span>
 </div>
