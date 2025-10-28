c9af25eae77ace83b632df82d19d734f0635efd3
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-10-28 16:41:40 +0530

    added profile info update logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -11,5 +13,21 @@ class ProfileController extends Controller
     public function index(): View
     {
         return view("pages.profile");
+    }
+
+    // @desc Update User profile
+    public function update(Request $request): RedirectResponse
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
+            // 'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $user->update($validatedData);
+
+        return redirect()->route('profile')->with('success', 'User Info Updated Successfully');
     }
 }
