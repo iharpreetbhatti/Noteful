7ce00840624c108cce02b0f0477460febe48e41f
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-11-05 17:18:52 +0530

    created config file with predefined notes color options

---
+++ config/notecolors.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  ['name' => 'White', 'value' => 'bg-white'],
+  ['name' => 'Slate', 'value' => 'bg-slate-50'],
+  ['name' => 'Emerald', 'value' => 'bg-emerald-50'],
+  ['name' => 'Amber', 'value' => 'bg-amber-50'],
+  ['name' => 'Violet', 'value' => 'bg-violet-50'],
+  ['name' => 'Rose', 'value' => 'bg-rose-50'],
+  ['name' => 'Orange', 'value' => 'bg-orange-50'],
+  ['name' => 'Blue', 'value' => 'bg-blue-50'],
+  ['name' => 'Pink', 'value' => 'bg-pink-50'],
+];
