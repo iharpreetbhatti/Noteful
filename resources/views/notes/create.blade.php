dfaaf973bc5feef24bee8d879520407eefd442f4
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-11-05 17:23:23 +0530

    added color selector component to note creation form

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -7,7 +7,10 @@
       <x-inputs.textarea name="description" placeholder="Start writing your note"
         class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description') }}" />
 
-      <button type="submit" class="bg-black text-white px-4 py-2 rounded"><i class="fa-regular fa-floppy-disk"></i>
+      <x-color-selector />
+
+      <button type="submit" class="block bg-black text-white px-4 py-2 rounded"><i
+          class="fa-regular fa-floppy-disk"></i>
         Save</button>
     </form>
   </div>
