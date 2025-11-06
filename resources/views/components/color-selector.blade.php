fc16e87ea9f648af9223b1e57428448dfa21ef62
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-11-06 14:36:27 +0530

    altered color selection logic for create form

--- resources/views/components/color-selector.blade.php
+++ resources/views/components/color-selector.blade.php
@@ -1,26 +1,25 @@
 {{-- TODO: Implement Color class binding on selection --}}
 {{-- TODO: Implement selected color input attachment to form data --}}
 
-@php
-  $colors = json_encode(config('notecolors'));
-@endphp
-
-<div class="inline-block relative mb-8" x-data="{ open: false, colors: {{ $colors }}, selectedColor: 'bg-white' }">
-  <div class="cursor-pointer border border-gray-300 p-2 rounded-2xl" @click="open = !open">
+<div class="inline-block relative mb-8" x-data="{ open: false }">
+  <div class="cursor-pointer border border-gray-300 p-2 rounded-2xl bg-transparent" @click="open = !open">
     <i class="fa-solid fa-palette fa-xl"></i>
   </div>
 
-  <div class="flex items-center justify-between gap-2 absolute top-0 left-16 p-2 rounded-xl bg-grey-100 shadow"
+  <div class="flex items-center justify-between gap-2 absolute top-0 left-16 p-2 rounded-xl bg-grey-100 shadow bg-white"
     x-show="open" @click.away="open = false">
-    <template x-for="color in colors" :key="color.value">
-      <span class="w-6 h-6 rounded-full border border-gray-500 cursor-pointer" :class="color.value"
-        :title="color.name"
+    <template x-for="color in colors" :key="color">
+      <span
+        class="w-6 h-6 rounded-full border border-gray-500 hover:ring hover:ring-offset-1 hover:ring-blue-500 cursor-pointer"
+        :class="color === 'white' ? 'bg-white' : `bg-${color}-50`"
+        :title="color.charAt(0).toUpperCase() + color.slice(1)"
         @click="
-            selectedColor = color.value;
+            selectedColor = color;
             open = false;
-            $el.querySelector('input[type=hidden]').value = color.value;
+            {{-- $el.querySelector('input[type=hidden]').value = color.value; --}}
           ">
       </span>
     </template>
   </div>
 </div>
+<input type="hidden" name="color" :value="selectedColor">
--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,17 +1,23 @@
+{{-- @php
+  dd(config('notecolors'));
+@endphp --}}
 <x-layout>
-  <div class="mx-auto mb-4 p-8 rounded-lg md:w-3xl bg-white">
-    <form action="/notes" method="POST">
-      @csrf
-      <x-inputs.text name="title" placeholder="Note Title" class="p-2 w-full mb-2 focus:outline-none"
-        value="{{ old('title') }}" />
-      <x-inputs.textarea name="description" placeholder="Start writing your note"
-        class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description') }}" />
+  <div x-data="{ colors: {{ json_encode(config('notecolors')) }}, selectedColor: 'white' }">
+    <div class="mx-auto mb-4 p-8 rounded-lg md:w-3xl"
+      :class="selectedColor === 'white' ? 'bg-white' : `bg-${selectedColor}-50`">
+      <form action="/notes" method="POST">
+        @csrf
+        <x-inputs.text name="title" placeholder="Note Title" class="p-2 w-full mb-2 focus:outline-none"
+          value="{{ old('title') }}" />
+        <x-inputs.textarea name="description" placeholder="Start writing your note"
+          class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description') }}" />
 
-      <x-color-selector />
+        <x-color-selector />
 
-      <button type="submit" class="block bg-black text-white px-4 py-2 rounded"><i
-          class="fa-regular fa-floppy-disk"></i>
-        Save</button>
-    </form>
+        <button type="submit" class="block bg-black text-white px-4 py-2 rounded cursor-pointer"><i
+            class="fa-regular fa-floppy-disk"></i>
+          Save</button>
+      </form>
+    </div>
   </div>
 </x-layout>
