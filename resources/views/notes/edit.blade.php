eeb83028d50d407b2c9b44a1c8ea9e1453a1f97f
Harpreet Singh <harpreetbhattiofficial@gmail.com>
2025-11-08 15:43:35 +0530

    added color selection field to note edit form

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,16 +1,24 @@
 <x-layout>
-  <div
-    class="mx-auto mb-4 p-8 rounded-xl md:w-3xl {{ $note->color === 'white' ? 'bg-white' : 'bg-' . $note->color . '-50' }}">
-    <form action="{{ route('notes.update', $note->id) }}" method="POST">
-      @csrf
-      @method('PUT')
-      <x-inputs.text name="title" placeholder="Note Title" class="p-2 w-full mb-2 focus:outline-none"
-        value="{{ old('title', $note->title) }}" />
-      <x-inputs.textarea name="description" placeholder="Start writing your note"
-        class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description', $note->description) }}" />
+  <div x-data="{
+      colors: {{ json_encode(config('notecolors')) }},
+      selectedColor: '{{ $note->color }}'
+  }">
+    <div class="mx-auto mb-4 p-8 rounded-xl md:w-3xl"
+      :class="selectedColor === 'white' ? 'bg-white' : `bg-${selectedColor}-50`">
+      <form action="{{ route('notes.update', $note->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <x-inputs.text name="title" placeholder="Note Title" class="p-2 w-full mb-2 focus:outline-none"
+          value="{{ old('title', $note->title) }}" />
+        <x-inputs.textarea name="description" placeholder="Start writing your note"
+          class="p-2 w-full mb-2 focus:outline-none" value="{{ old('description', $note->description) }}" />
 
-      <button type="submit" class="bg-black text-white px-4 py-2 rounded"><i class="fa-regular fa-floppy-disk"></i>
-        Save</button>
-    </form>
+        <x-color-selector />
+
+        <button type="submit" class="block bg-black text-white px-4 py-2 rounded"><i
+            class="fa-regular fa-floppy-disk"></i>
+          Save</button>
+      </form>
+    </div>
   </div>
 </x-layout>
